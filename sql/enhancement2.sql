2f729f96f27ea263e0a280ecc0a81861ab3d9202
Nate J <46363319+Millwr1ght@users.noreply.github.com>
2023-05-10 12:22:11 -0500

    Update enhancement2.sql

--- sql/enhancement2.sql
+++ sql/enhancement2.sql
@@ -1,8 +1,8 @@
 USE phpmotors;
 
 -- add tony stark to the client table
-INSERT INTO TABLE client ('first_name', 'last_name', 'email', 'password', 'comment')
-VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n', "I am the real Ironman");
+INSERT INTO client (first_name, last_name, email, password, comment)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n', 'I am the real Ironman');
 
 -- update tony stark's client level
 UPDATE client
@@ -17,16 +17,16 @@ WHERE invMake = 'GM' and invModel = 'Hummer';
 -- use an INNER JOIN to SELECT the invModel field from the inventory 
 -- table and the classificationName field from the carclassification table
 -- for items that belong to the SUV category
-SELECT i.invModel and c.classificationName
-FROM inventory i INNER JOIN carclassification
+SELECT i.invModel, c.classificationName
+FROM inventory i INNER JOIN carclassification c
 ON i.classificationID = c.classificationID
 WHERE c.classificationName = 'SUV';
 
 -- drop the jeep wrangler
-DELETE FROM `inventory`
+DELETE FROM inventory
 WHERE invMake = 'Jeep' AND invModel = 'Wrangler';
 
 -- use update to prepend '/phpmotors' to each image file path on the inventory table
 UPDATE inventory
 SET invImage = CONCAT('/phpmotors', invImage), invThumbnail = CONCAT('/phpmotors', invThumbnail)
-WHERE 1
+WHERE 1;
