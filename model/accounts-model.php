4173146142a2603b924d91c56b3a31d6708c0824
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2023-05-19 13:07:23 -0500

    begin enhancement 4: accounts

---
+++ model/accounts-model.php
@@ -0,0 +1,34 @@
+<?php // this is the accounts model, as evidenced by the name of the file.
+
+// site registration
+function regClient($clientFirstname, $clientLastname, $clientEmail, $clientPassword){
+
+    //connect to database
+    $db = phpmotorsConnect();
+
+    //prepare squirrels
+    $sql = 'INSERT INTO clients (cliestFirstname, clientLastname, clientEmail, clientPassword) VALUES (:clientFirstname,:clientLastname,:clientEmail,:clientPassword);';
+
+    //combine statement with connection
+    $stmt = $db->prepare($sql);
+
+    //replace placeholders
+    $stmt->bindValue(':clientFirstname', $clientFirstname,PDO::PARAM_STR);
+    $stmt->bindValue(':clientLastname', $clientLastname,PDO::PARAM_STR);
+    $stmt->bindValue(':clientEmail', $clientEmail,PDO::PARAM_STR);
+    $stmt->bindValue(':clientPassword', $clientPassword,PDO::PARAM_STR);
+
+    //eggsecute the squirrels
+    $stmt->execute();
+
+    //hopefully only one row changes
+    $rowsChanged = $stmt->rowCount();
+
+    //end interaction
+    $stmt->closeCursor();
+
+    //success if 1 (true), fail if more or less true (n < 1 < n)
+    return $rowsChanged;
+}
+
+?>
